008b913f346e20efb200c3c075d2f7dd5871cb4b
Tatyana Kazakova <tatikzk@MBP-Tatyana.Dlink>
2023-06-20 23:27:19 +0300

    other

--- niffler-e-2-e-tests-hw/src/test/java/guru/qa/niffler/dbHelper/entity/authEntity/Authority.java
+++
@@ -1,5 +0,0 @@
-package guru.qa.niffler.dbHelper.entity.authEntity;
-
-public enum Authority {
-    read, write
-}
--- niffler-e-2-e-tests-hw/src/test/java/guru/qa/niffler/test/exampleJunit/BaseTest.java
+++
@@ -1,9 +0,0 @@
-package guru.qa.niffler.test.exampleJunit;
-
-import guru.qa.niffler.jupiter.extension.exampleJunitExtension.ExampleSuiteExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-@ExtendWith(ExampleSuiteExtension.class)
-public abstract class BaseTest {
-
-}
